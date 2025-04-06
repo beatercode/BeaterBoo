c670dbee9c85b6e821f43e0a18003b84bef3323b
beatercode <minoi.luca@gmail.com>
2025-04-06 17:26:59 +0200

    Convertito webpack.config.js a ES modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   mode: 'production',
   entry: './src/main.tsx',
   output: {
